1294c2f772c503dafc8ddba082ebecdc504c6c54
ada.young <cng1985@gmail.com>
2015-12-04 18:23:06 +0800

    修改目录

--- src/main/java/com/ada/admin/entity/Menu.java
+++ src/main/java/com/ada/admin/entity/Menu.java
@@ -1,11 +1,14 @@
 package com.ada.admin.entity;
 
+import java.util.List;
+
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
-import com.ada.article.entity.ArticleCatalog;
 import com.ada.data.entity.CatalogEntity;
 
 @Entity
@@ -14,14 +17,37 @@ public class Menu extends CatalogEntity {
 
 	@JoinColumn(name = "pid")
 	@ManyToOne
-	private ArticleCatalog parent;
+	private Menu parent;
 
-	public ArticleCatalog getParent() {
-		return parent;
+	/**
+	 * 图标
+	 */
+	private String icon;
+
+	/**
+	 * url地址
+	 */
+	private String path;
+
+	@OneToMany(mappedBy = "parent", fetch = FetchType.EAGER)
+	private List<Menu> childrens;
+
+	public List<Menu> getChildrens() {
+		if (childrens != null && childrens.size() > 0) {
+			return childrens;
+
+		} else {
+			return null;
+
+		}
 	}
 
-	public void setParent(ArticleCatalog parent) {
-		this.parent = parent;
+	public String getIcon() {
+		return icon;
+	}
+
+	public Menu getParent() {
+		return parent;
 	}
 
 	@Override
@@ -31,5 +57,25 @@ public class Menu extends CatalogEntity {
 		} else {
 			return null;
 		}
+	}
+
+	public String getPath() {
+		return path;
+	}
+
+	public void setChildrens(List<Menu> childrens) {
+		this.childrens = childrens;
+	}
+
+	public void setIcon(String icon) {
+		this.icon = icon;
+	}
+
+	public void setParent(Menu parent) {
+		this.parent = parent;
+	}
+
+	public void setPath(String path) {
+		this.path = path;
 	}
 }
