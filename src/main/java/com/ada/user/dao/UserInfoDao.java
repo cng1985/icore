0f63cd9a5b61b74deb8fa58105677832e3c428b8
ada.young <cng1985@gmail.com>
2015-12-21 18:13:13 +0800

    修改用户注册

--- src/main/java/com/ada/user/dao/UserInfoDao.java
+++ src/main/java/com/ada/user/dao/UserInfoDao.java
@@ -1,12 +1,26 @@
 package com.ada.user.dao;
 
-
-import  com.ada.data.core.BaseDao;
-import  com.ada.data.core.Updater;
+import com.ada.data.core.BaseDao;
+import com.ada.data.core.Updater;
 import com.ada.data.core.Pagination;
-import  com.ada.user.entity.UserInfo;
+import com.ada.user.entity.UserInfo;
 
-public interface UserInfoDao extends BaseDao<UserInfo, Long>{
+public interface UserInfoDao extends BaseDao<UserInfo, Long> {
+
+	/** 加密方法 */
+	public static final String HASH_ALGORITHM = "SHA-1";
+	public static final int HASH_INTERATIONS = 1024;
+	public static final int SALT_SIZE = 8; // 盐长度
+
+	/**
+	 * 
+	 * @param username
+	 * @param password
+	 * @param email
+	 * @return -1为用户已存在 1 注册成功
+	 */
+	public UserInfo register(String username, String password, String email);
+
 	public Pagination getPage(int pageNo, int pageSize);
 
 	public UserInfo findById(Long id);
--- src/main/java/com/ada/user/dao/impl/UserInfoDaoImpl.java
+++ src/main/java/com/ada/user/dao/impl/UserInfoDaoImpl.java
@@ -1,12 +1,17 @@
 package com.ada.user.dao.impl;
 
+import java.util.List;
+
 import org.hibernate.Criteria;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ada.data.core.BaseDaoImpl;
+import com.ada.data.core.Finder;
 import com.ada.data.core.Pagination;
+import com.ada.security.Digests;
+import com.ada.security.Encodes;
 import com.ada.user.dao.UserInfoDao;
 import com.ada.user.entity.UserInfo;
 
@@ -35,14 +40,63 @@ public class UserInfoDaoImpl extends BaseDaoImpl<UserInfo, Long> implements User
 		}
 		return entity;
 	}
-	
+
 	@Override
 	protected Class<UserInfo> getEntityClass() {
 		return UserInfo.class;
 	}
-	
+
 	@Autowired
-	public void setSuperSessionFactory(SessionFactory sessionFactory){
-	    super.setSessionFactory(sessionFactory);
+	public void setSuperSessionFactory(SessionFactory sessionFactory) {
+		super.setSessionFactory(sessionFactory);
 	}
+
+	/**
+	 * 验证原密码是否正确
+	 * 
+	 * @param user
+	 * @param oldPwd
+	 * @return
+	 */
+	public boolean checkPassword(UserInfo user, String oldPassword) {
+		byte[] salt = Encodes.decodeHex(user.getSalt());
+		byte[] hashPassword = Digests.sha1(oldPassword.getBytes(), salt, HASH_INTERATIONS);
+		if (user.getPassword().equals(Encodes.encodeHex(hashPassword))) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * 设定安全的密码，生成随机的salt并经过1024次 sha-1 hash
+	 */
+	private void entryptPassword(UserInfo user) {
+		byte[] salt = Digests.generateSalt(SALT_SIZE);
+		user.setSalt(Encodes.encodeHex(salt));
+
+		byte[] hashPassword = Digests.sha1(user.getPlainPassword().getBytes(), salt, HASH_INTERATIONS);
+		user.setPassword(Encodes.encodeHex(hashPassword));
+	}
+
+	@Override
+	public UserInfo register( String username, String password,String email) {
+		UserInfo result = new UserInfo();
+		Finder finder = Finder.create();
+		finder.append("from UserInfo u where u.username ='" + username + "'");
+		// finder.append(" and u.password = '" + password + "'");
+		List<UserInfo> us = find(finder);
+		if (us != null && us.size() > 0) {
+			result.setId(-1l);
+		} else {
+			UserInfo bean = new UserInfo();
+			bean.setUsername(username);
+			bean.setPlainPassword(password);
+			bean.setEmail(email);
+			entryptPassword(bean);
+			result=save(bean);
+		}
+		return result;
+	}
+
 }
