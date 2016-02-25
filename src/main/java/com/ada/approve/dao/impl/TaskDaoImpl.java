e73378b8851bbf717bea6355e2cdf2865ed008d2
ada.young <cng1985@gmail.com>
2016-02-25 16:55:24 +0800

    修改审批

--- src/main/java/com/ada/approve/dao/impl/TaskDaoImpl.java
+++ src/main/java/com/ada/approve/dao/impl/TaskDaoImpl.java
@@ -1,14 +1,19 @@
 package com.ada.approve.dao.impl;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import org.hibernate.Criteria;
+import org.hibernate.SQLQuery;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
-import com.ada.data.core.CriteriaDaoImpl;
-import com.ada.data.core.Pagination;
 import com.ada.approve.dao.TaskDao;
 import com.ada.approve.entity.Task;
+import com.ada.data.core.CriteriaDaoImpl;
+import com.ada.data.core.Pagination;
 
 @Repository
 public class TaskDaoImpl extends CriteriaDaoImpl<Task, Long> implements TaskDao {
@@ -35,14 +40,32 @@ public class TaskDaoImpl extends CriteriaDaoImpl<Task, Long> implements TaskDao 
 		}
 		return entity;
 	}
-	
+
 	@Override
 	protected Class<Task> getEntityClass() {
 		return Task.class;
 	}
-	
+
 	@Autowired
-	public void setSuperSessionFactory(SessionFactory sessionFactory){
-	    super.setSessionFactory(sessionFactory);
+	public void setSuperSessionFactory(SessionFactory sessionFactory) {
+		super.setSessionFactory(sessionFactory);
+	}
+
+	public List<Date> list(long uid, int year, int month) {
+		List<Date> result = new ArrayList<Date>();
+		SQLQuery query = getSession().createSQLQuery(
+				"select * from approve_task where year(approve_task.addDate)=:cyear and month(approve_task.addDate)=:cmonth and approve_task.user_id =:uid group by date_format(approve_task.addDate,'%Y-%m-%d')");
+
+		query.setParameter("cyear", year);
+		query.setParameter("cmonth", month);
+		query.setParameter("uid", uid);
+		List<Task> tasks = query.addEntity(Task.class).list();
+		if (tasks != null) {
+			for (Task task : tasks) {
+				result.add(task.getAddDate());
+			}
+		}
+		return result;
+
 	}
 }
