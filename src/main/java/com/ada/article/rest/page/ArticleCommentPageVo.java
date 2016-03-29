e7548d168d1690288e5367c24afc533f44f3bb57
ada.young <cng1985@gmail.com>
2016-03-29 22:59:03 +0800

    修改文章评论

--- src/main/java/com/ada/article/rest/page/ArticleCommentPageVo.java
+++ src/main/java/com/ada/article/rest/page/ArticleCommentPageVo.java
@@ -1,8 +1,19 @@
 package com.ada.article.rest.page;
 
+import java.util.List;
+
 import com.ada.article.rest.vo.ArticleCommentVo;
 import com.ada.data.dto.PageRpc;
 
-public class ArticleCommentPageVo extends PageRpc<ArticleCommentVo> {
+public class ArticleCommentPageVo {
+	private List<ArticleCommentVo> list;
+
+	public List<ArticleCommentVo> getList() {
+		return list;
+	}
+
+	public void setList(List<ArticleCommentVo> list) {
+		this.list = list;
+	}
 
 }
