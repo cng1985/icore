2215df5825c5bf676de91a0c8cb107d0bbb920ea
ada.young <cng1985@gmail.com>
2016-08-10 11:20:11 +0800

    增加用户实体

---
+++ src/main/java/com/ada/data/entity/UserEntity.java
@@ -0,0 +1,30 @@
+package com.ada.data.entity;
+
+import javax.persistence.FetchType;
+import javax.persistence.ManyToOne;
+
+import com.ada.user.entity.UserInfo;
+
+/**
+ * 数据带用户信息的实体
+ * 
+ * @author ada
+ *
+ */
+public class UserEntity extends AbstractEntity {
+
+	/**
+	 * 用户
+	 */
+	@ManyToOne(fetch = FetchType.LAZY)
+	private UserInfo user;
+
+	public UserInfo getUser() {
+		return user;
+	}
+
+	public void setUser(UserInfo user) {
+		this.user = user;
+	}
+
+}
